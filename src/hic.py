674f34454c09bcea21883b62a6a75c6ebd9abc40
Nick Barkas <nikolas.barkas@outlook.com>
2019-08-06 11:01:36 -0400

    delete copy of python files; update main src files with python line

--- src/hic.py
+++ src/hic.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from weakref import WeakValueDictionary
 
 import numpy as np
--- wdl/docker/build.sh
+++ wdl/docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cp -r ../../src .
 docker build -t quay.io/nbarkas/abc-general-container .
+rm -rf src
 echo You can now push the image with "docker push quay.io/nbarkas/abc-general-container:latest"
