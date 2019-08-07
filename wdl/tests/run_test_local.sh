d0f4817a86105dc0b0fb6a20512e3181c17fe8c4
Nick Barkas <nikolas.barkas@outlook.com>
2019-08-07 11:12:45 -0400

    update cloud tests

---
+++ wdl/tests/run_test_local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Run the tests on the cloud. This requires a local cromwell that has access to google api
+## See instructions here to configure this: https://cromwell.readthedocs.io/en/develop/tutorials/PipelinesApi101/
+## and add the google.conf file to the current directory
+
+CROMWELL_JAR_PATH=/Users/nbarkas/jars/cromwell-44.jar
+
+java -Dconfig.file=google.conf -jar $CROMWELL_JAR_PATH run test_ABC.wdl -i test_inputs.json
