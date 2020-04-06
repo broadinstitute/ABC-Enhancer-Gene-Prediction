a0d4030bfdd75bca53a95dc5dbb74e75e4023ea5
kmualim <kristy.mualim@mail.mcgill.ca>
2020-04-06 16:56:40 -0700

    clean up metrics

--- src/metrics.py
+++ src/metrics.py
@@ -7,16 +7,28 @@ from subprocess import check_call, check_output, PIPE, Popen, getoutput, CalledP
 
 def grab_nearest_tss_from_peak(macs_peaks, genome_tss, outdir):
     # Grab nearest tss from peak
-    outfile = os.path.join(outdir, os.path.basename(macs_peaks) + ".gz.candidateRegions.bed")
+    outfile = macs_peaks
     files = pd.read_csv(outfile, sep="\t")
     annotated_peaks = os.path.join(outdir, os.path.basename(macs_peaks) + ".annotated_peaks.bed")
-    sort_command = "sort -k1,1 -k2,2n {outfile} > {outfile}.sorted"
+    if outfile.endswith(".gz"):
+        sort_command = "zcat {outfile} | sort -k1,1 -k2,2n > {outfile}.sorted"
+    else:
+        sort_command = "sort -k1,1 -k2,2n {outfile} > {outfile}.sorted"
     sort_command = sort_command.format(**locals())
     p = Popen(sort_command, stdout=PIPE, stderr=PIPE, shell=True)
     print("Running:" + sort_command)
     (stdoutdata, stderrdata) = p.communicate()
     err = str(stderrdata, 'utf-8')
-    command = "bedtools closest -a {outfile}.sorted -b {genome_tss} -d > {annotated_peaks}"
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    sort_genome_command = "sort -k1,1 -k2,2n {genome_tss} > {genome_tss}.sorted"
+    sort_genome_command = sort_genome_command.format(**locals())
+    p = Popen(sort_genome_command, stdout=PIPE, stderr=PIPE, shell=True)
+    print("Running:" + sort_genome_command)
+    (stdoutdata, stderrdata) = p.communicate()
+    err = str(stderrdata, 'utf-8')
+
+    command = "bedtools closest -a {outfile}.sorted -b {genome_tss}.sorted -d > {annotated_peaks}"
     command = command.format(**locals())
     p = Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
     print("Running:" + command)
@@ -25,37 +37,37 @@ def grab_nearest_tss_from_peak(macs_peaks, genome_tss, outdir):
 
     return stdoutdata
 
+def grabStatistics(arr_values):
+    mean = arr_values.mean()
+    median = arr_values.median()
+    std = arr_values.std()
+    return mean, median, std
+
 # Generates QC Prediction Metrics:
 def GrabQCMetrics(prediction_df, outdir):
-    GeneCounts = prediction_df.groupby(['TargetGene']).size()
-    GeneCounts.to_csv(os.path.join(outdir,"EnhancerPerGene.txt"), sep="\t")
+    EnhancerPerGene = prediction_df.groupby(['TargetGene']).size()
+    EnhancerPerGene.to_csv(os.path.join(outdir,"EnhancerPerGene.txt"), sep="\t")
 
     # Grab Number of Enhancers Per Gene
-    GeneMedian = prediction_df.groupby(['TargetGene']).size().median()
-    GeneMean = prediction_df.groupby(['TargetGene']).size().mean()
-    GeneStdev = prediction_df.groupby(['TargetGene']).size().std()
+    GeneMean, GeneMedian, GeneStdev = grabStatistics(EnhancerPerGene)
 
     # Grab Number of genes per enhancers
-    num_enhancers = prediction_df[['chr', 'start', 'end']].groupby(['chr', 'start', 'end']).size()
-    num_enhancers.to_csv(os.path.join(outdir,"GenesPerEnhancer.txt"), sep="\t")
-    mean_genes_per_enhancer = prediction_df[['chr', 'start', 'end']].groupby(['chr', 'start', 'end']).size().mean()
-    stdev_genes_per_enhancer = prediction_df[['chr', 'start', 'end']].groupby(['chr', 'start', 'end']).size().std()
-    median_genes_per_enhancer = prediction_df[['chr', 'start', 'end']].groupby(['chr', 'start', 'end']).size().median()
+    NumGenesPerEnhancer = prediction_df[['chr', 'start', 'end']].groupby(['chr', 'start', 'end']).size()
+    NumGenesPerEnhancer.to_csv(os.path.join(outdir,"GenesPerEnhancer.txt"), sep="\t")
+    mean_genes_per_enhancer, median_genes_per_enhancer,stdev_genes_per_enhancer = grabStatistic(NumGenesPerEnhancer) 
+
     # Grab Number of Enhancer-Gene Pairs Per Chromsome
     enhancergeneperchrom = prediction_df.groupby(['chr']).size()
     enhancergeneperchrom.to_csv(os.path.join(outdir, "EnhancerGenePairsPerChrom.txt"), sep="\t")
-    mean_enhancergeneperchrom = prediction_df.groupby(['chr']).size().mean()
-    stdev_enhancergeneperchrom = prediction_df.groupby(['chr']).size().std()
-    median_enhancergeneperchrom = prediction_df.groupby(['chr']).size().median()
+    
+    mean_enhancergeneperchrom, median_enhancergeneperchrom ,stdev_enhancergeneperchrom = grabStatistic(enhancergeneperchrom) 
 
     # Enhancer-Gene Distancee
     distance = np.array(prediction_df['distance'])
     thquantile = np.percentile(distance, 10)
     testthquantile = np.percentile(distance, 90)
 
-    # Quantile Normalization Plots
     
-
     # Plot Distributions and save as png
     PlotDistribution(num_enhancers, "NumberOfGenesPerEnhancer", outdir)
     PlotDistribution(GeneCounts, "NumberOfEnhancersPerGene", outdir)
@@ -123,10 +135,9 @@ def PlotQuantilePlot(EnhancerList, title, outdir):
     
 
 def NeighborhoodFileQC(neighborhood_dir, outdir):
-    x = glob.glob(os.path.join(neighborhood_dir, "Enhancers.DHS.*"))
-    y = glob.glob(os.path.join(neighborhood_dir, "Genes.TSS1kb.DHS.*"))
-    z = glob.glob(os.path.join(neighborhood_dir, "Genes.DHS.*"))
-    
+    x = glob.glob(os.path.join(neighborhood_dir, "Enhancers.DHS.*CountReads.bedgraph"))
+    y = glob.glob(os.path.join(neighborhood_dir, "Genes.TSS1kb.DHS.*CountReads.bedgraph"))
+    z = glob.glob(os.path.join(neighborhood_dir, "Genes.DHS.*CountReads.bedgraph"))
     data = pd.read_csv(x[0],sep="\t", header=None)
     data1 = pd.read_csv(y[0],sep="\t", header=None)
     data2 = pd.read_csv(z[0],sep="\t", header=None)
@@ -142,6 +153,7 @@ def NeighborhoodFileQC(neighborhood_dir, outdir):
         f.write(str(counts2))
         f.write("\t")
         f.write(str(counts3))
+        f.write("\n")
         f.close()
 
 # Generates peak file metrics
@@ -150,23 +162,26 @@ def PeakFileQC(macs_peaks, outdir):
         peaks = pd.read_csv(macs_peaks, compression="gzip", sep="\t", header=None)
     else:
         peaks = pd.read_csv(macs_peaks, sep="\t", header=None)
-    outfile = os.path.join(outdir, os.path.basename(macs_peaks) + ".gz.candidateRegions.bed")
+    outfile = os.path.join(outdir, os.path.basename(macs_peaks) + ".candidateRegions.bed")
+    if not os.path.exists(outfile):
+        outfile = os.path.join(outdir, os.path.basename(macs_peaks) + ".gz.candidateRegions.bed")
+    
     candidateRegions = pd.read_csv(outfile, sep="\t", header=None)
     candidateRegions['dist'] = candidateRegions[2] - candidateRegions[1]
     candreg = list(candidateRegions['dist'])
-#    PlotDistribution(candreg, 'WidthOfCandidateRegions', outdir)
+    PlotDistribution(candreg, 'WidthOfCandidateRegions', outdir)
 
     annotatedFile = os.path.join(outdir, os.path.basename(macs_peaks) + ".annotated_peaks.bed")
     annotatedPeaks = pd.read_csv(annotatedFile, sep="\t", header=None)
-    median = annotatedPeaks.iloc[:, 9].median()
-    mean = annotatedPeaks.iloc[:, 9].mean()
-    stdev = np.std(np.array(annotatedPeaks.iloc[:, 9]))
+    annote = annotatedPeaks.loc[annotatedPeaks.iloc[:,9]!=-1]
+    mean, median, stdev = grabStatistics(annote.iloc[:,9])
+
 #    PlotDistribution(np.array(annotatedPeaks.iloc[:,9]), "DistanceOfPeakToClosestTSS", outdir)
 
     peaks['dist'] = peaks[2]-peaks[1]
     peaks_array = list(peaks['dist'])
-#    PlotDistribution(peaks_array, "WidthOfPeaks", outdir)
 
+#    PlotDistribution(peaks_array, "WidthOfPeaks", outdir)
     with open(os.path.join(outdir, "PeakFileQCSummary.txt"),"w") as f:
         f.write(str(macs_peaks))
         f.write("\n")
@@ -191,11 +206,11 @@ def PeakFileQC(macs_peaks, outdir):
         f.write(str(len(candidateRegions['dist'])))
         f.write("\n")
         f.write("Median width of Candidate Regions:")
-        f.write(str((peaks['dist'].median())))
+        f.write(str((candidateRegions['dist'].median())))
         f.write("\t")
-        f.write(str(peaks['dist'].mean()))
+        f.write(str(candidateRegions['dist'].mean()))
         f.write("\t")
-        f.write(str(peaks['dist'].std()))
+        f.write(str(candidateRegions['dist'].std()))
         f.write("\n")
         f.close()
 
