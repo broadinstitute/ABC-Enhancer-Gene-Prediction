bff451fba7292d005973f31577b901b556e10c1a
jnasser <jnasser@broadinstitute.org>
2020-05-25 17:21:53 -0400

    factor out macs2 sorting. Other broad related updates

--- Snakefiles/workflow/rules/RunENCODESnakemake.Broad.sh
+++ Snakefiles/workflow/rules/RunENCODESnakemake.Broad.sh
@@ -3,12 +3,15 @@
 ## Jesse Engreitz
 ## May 11, 2020
 
-PROJECT=/seq/lincRNA/RAP/ABC/200330_ENCODE; cd $PROJECT
+PROJECT=/seq/lincRNA/RAP/ABC/200330_ENCODE/200525; cd $PROJECT
 CODEDIR=$PROJECT/ABC-Enhancer-Gene-Prediction/
 
+#Activate conda environment
+conda activate /seq/lincRNA/RAP/ABC/200330_ENCODE/env/abcenv
+
 #source /broad/software/scripts/useuse
-reuse .python-3.5.1 .samtools-1.8 .tabix-0.2.5 BEDTools
-source /seq/lincRNA/Ben/VENV_MIP/bin/activate
+# reuse .python-3.5.1 .samtools-1.8 .tabix-0.2.5 BEDTools
+# source /seq/lincRNA/Ben/VENV_MIP/bin/activate
 
 #SAMPLESHEET=$1
 #CONFIG=$2 
@@ -35,3 +38,14 @@ snakemake -s "$CODEDIR/Snakefiles/workflow/rules/preprocess/Snakefile" \
 	## EDIT THIS: --config working_dir=$PROJECT download_dir=$PROJECT/download output_data_dir=$PROJECT/download \
 	 &> logs/download.out &
 
+
+##########################################################################
+## Run 'ABC' snakefile
+## TO EDIT Joe
+snakemake -s "$CODEDIR/Snakefiles/workflow/rules/abc_code/Snakefile" \
+	--configfile $CODEDIR/Snakefiles/workflow/envs/wd_Broad.yaml \
+	--config working_dir=$PROJECT download_dir=$PROJECT/download output_data_dir=$PROJECT/download \
+			predictions_results_dir=$PROJECT/abc_out \
+			hic_dir=/seq/lincRNA/RAP/External/Rao2014-HiC/190924_new_structure/hic/191020_Average/out/ \
+		#--profile $CODEDIR/Snakefiles/workflow/envs/BroadGSA/ \
+	 &> logs/download.out &
