bd90c88c636e90cd3de49e4a3c7e4542fe3a7a6a
Anthony Tan <atan5133@gmail.com>
2023-06-02 12:09:12 -0700

    Cleanup snakemake file

---
+++ runABC.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+snakemake --use-conda -j1
--- workflow/runABC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CODEDIR=/oak/stanford/groups/engreitz/Users/atan5133/ABC-Enhancer-Gene-Prediction
-
-snakemake -s $CODEDIR/workflow/Snakefile_working --configfile $CODEDIR/config/config.yaml --directory $CODEDIR/workflow --unlock 
-
-snakemake -s $CODEDIR/workflow/Snakefile_working --configfile $CODEDIR/config/config.yaml --use-conda -j1 --conda-frontend conda --keep-target-files --rerun-incomplete
