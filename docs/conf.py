86720bd2fb773cd2bb9518a006655196391f50b1
Anthony Tan <atan5133@gmail.com>
2023-06-27 14:44:57 -0700

    testing deletion of file

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = "Anthony Tan"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = []  # type: ignore
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
