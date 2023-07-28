f0b032c7f93301a8f3fc53fc29863870bf5659e7
Anthony Tan <atan5133@gmail.com>
2023-07-28 13:06:12 -0700

    add hic_resolution. added snakemake input validation. added more ref files

--- workflow/scripts/write_powerlaw_params.py
+++ workflow/scripts/write_powerlaw_params.py
@@ -4,7 +4,9 @@ import pandas as pd
 import subprocess
 
 
-def exec_compute_powerlaw_fit_script(hic_dir: str, hic_type: str, output_file: str):
+def exec_compute_powerlaw_fit_script(
+    hic_dir: str, hic_type: str, hic_resolution: str, output_file: str
+):
     output_dir = os.path.dirname(output_file)
     subprocess.run(
         [
@@ -14,35 +16,32 @@ def exec_compute_powerlaw_fit_script(hic_dir: str, hic_type: str, output_file: s
             hic_dir,
             "--hic_type",
             hic_type,
+            "--hic_resolution",
+            str(hic_resolution),
             "--outDir",
             output_dir,
         ]
     )
 
 
-def is_float(string):
-    try:
-        float_value = float(string)
-        return True
-    except ValueError:
-        return False
-
-
 @click.command()
 @click.option("--hic_dir", type=str)
 @click.option("--hic_type", type=str)
-@click.option("--hic_gamma", type=str)
-@click.option("--hic_scale", type=str)
+@click.option("--hic_resolution", type=str)
+@click.option("--hic_gamma", type=float)
+@click.option("--hic_scale", type=float)
 @click.option("--output_file", type=str)
-def main(hic_dir, hic_type, hic_gamma, hic_scale, output_file):
-    if is_float(hic_gamma) and is_float(hic_scale):
+def main(hic_dir, hic_type, hic_resolution, hic_gamma, hic_scale, output_file):
+    if hic_gamma and hic_scale:
         pd.DataFrame({"hic_gamma": [hic_gamma], "hic_scale": [hic_scale]}).to_csv(
             output_file, sep="\t", index=False
         )
     else:
-        if not hic_dir or not hic_type:
-            raise click.MissingParameter("Must provide gamma/scale values or HiC dir")
-        exec_compute_powerlaw_fit_script(hic_dir, hic_type, output_file)
+        if not (hic_dir and hic_type and hic_resolution):
+            raise click.MissingParameter(
+                "Must provide gamma/scale values or full HiC info (dir,type, and resolution)"
+            )
+        exec_compute_powerlaw_fit_script(hic_dir, hic_type, hic_resolution, output_file)
 
 
 if __name__ == "__main__":
