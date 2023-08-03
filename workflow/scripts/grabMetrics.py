4b9c09ad5c8e42f4855329ed9ec37328d1427ec3
Anthony Tan <atan5133@gmail.com>
2023-08-03 15:02:00 -0700

    update QC plots

--- workflow/scripts/grabMetrics.py
+++ workflow/scripts/grabMetrics.py
@@ -1,38 +1,43 @@
 #! bin/python3
-import sys
-from metrics import *
-from tools import *
-import pandas as pd
-import glob
 import argparse
-import pickle
+import glob
 import os.path
+import pickle
+
+import pandas as pd
+from metrics import GrabQCMetrics, HiCQC, NeighborhoodFileQC, PeakFileQC
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--macs_peaks",
+        required=True,
         help="narrowPeak file output by macs2. (eg. /users/kmualim/K562/macs2_peaks.narrowPeak)",
     )
-    parser.add_argument("--preds_file", help="Prediction file output by ABC")
-    parser.add_argument("--neighborhood_outdir", help="Neighborhood Directory")
+    parser.add_argument(
+        "--preds_file", required=True, help="Prediction file output by ABC"
+    )
+    parser.add_argument(
+        "--neighborhood_outdir", required=True, help="Neighborhood Directory"
+    )
+    parser.add_argument("--chrom_sizes", required=True, help="Chromosome sizes file")
+    parser.add_argument("--outdir", required=True, help="Predictions Directory")
     parser.add_argument(
         "--powerlaw_params_tsv",
         type=str,
         help="TSV file containing gamma/scale values according to powerlaw fit",
     )
-    parser.add_argument("--outdir", help="Predictions Directory")
     args = parser.parse_args()
     return args
 
 
 def generateQCMetrics(args):
-    # prediction = "{}/EnhancerPredictionsFull.txt".format(args.preds_outdir)
+    chrom_order = pd.read_csv(args.chrom_sizes, sep="\t", header=None)[0].tolist()
     # read prediction file
     prediction_df = pd.read_csv(args.preds_file, sep="\t")
     # Generate QC Summary.txt in Predictions Directory
-    pred_metrics = GrabQCMetrics(prediction_df, args.outdir)
+    pred_metrics = GrabQCMetrics(prediction_df, chrom_order, args.outdir)
     # Generate PeakFileQCSummary.txt in Peaks Directory#
     pred_metrics = PeakFileQC(pred_metrics, args.macs_peaks, args.outdir)
     # Appends Percentage Counts in Promoters into PeakFileQCSummary.txt
