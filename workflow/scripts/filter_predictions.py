4bb5d22a71f016739b081bcdefb5b8dc3cadcd60
Anthony Tan <atan5133@gmail.com>
2023-08-10 15:39:59 -0700

    Reintroduce slim column enhancer prediction file

--- workflow/scripts/filter_predictions.py
+++ workflow/scripts/filter_predictions.py
@@ -3,9 +3,9 @@ import pandas as pd
 from tools import write_connections_bedpe_format
 from predictor import make_gene_prediction_stats
 
-
 @click.command()
 @click.option("--output_tsv_file", type=str)
+@click.option("--output_slim_tsv_file", type=str)
 @click.option("--output_bed_file", type=str)
 @click.option("--output_gene_stats_file", type=str)
 @click.option("--pred_file", type=str)
@@ -20,6 +20,7 @@ from predictor import make_gene_prediction_stats
 @click.option("--only_expressed_genes", type=bool)
 def main(
     output_tsv_file,
+    output_slim_tsv_file,
     output_bed_file,
     output_gene_stats_file,
     pred_file,
@@ -29,6 +30,8 @@ def main(
     include_self_promoter,
     only_expressed_genes,
 ):
+    slim_columns = ['chr','start','end','name','TargetGene','TargetGeneTSS','CellType', score_column]
+
     all_putative = pd.read_csv(pred_file, sep="\t")
     if not only_expressed_genes:
         non_expressed = pd.read_csv(pred_nonexpressed_file, sep="\t")
@@ -40,6 +43,11 @@ def main(
     filtered_predictions.to_csv(
         output_tsv_file, sep="\t", index=False, header=True, float_format="%.6f"
     )
+    filtered_predictions_slim = filtered_predictions[slim_columns]
+    filtered_predictions_slim.to_csv(
+        output_slim_tsv_file, sep="\t", index=False, header=True, float_format="%.6f"
+    )
+    
     write_connections_bedpe_format(filtered_predictions, output_bed_file, score_column)
     make_gene_prediction_stats(
         filtered_predictions, score_column, threshold, output_gene_stats_file
