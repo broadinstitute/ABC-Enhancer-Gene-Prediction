9534859ec02fedfe109dcba63230d58c52f8e087
Anthony Tan <10254642+atancoder@users.noreply.github.com>
2023-12-01 14:07:37 -0800

    update scATAC walkthrough (#160)

--- docs/conf.py
+++ docs/conf.py
@@ -22,4 +22,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_css_files = [
+    'custom.css',
+]
+
 html_theme = "sphinx_rtd_theme"
+
