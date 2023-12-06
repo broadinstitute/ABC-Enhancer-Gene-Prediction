5cea6d069a026493ed9fc98982493e2d8aa1073b
Anthony Tan <10254642+atancoder@users.noreply.github.com>
2023-12-06 13:03:26 -0800

    add script to extract avg hic (#165)

---
+++ workflow/scripts/extract_avg_hic.py
@@ -0,0 +1,39 @@
+import click
+import gzip
+import os
+import subprocess
+
+
+@click.command()
+@click.option("--avg_hic_bed_file", type=str, required=True)
+@click.option("--output_dir", type=str, default=".")
+def main(avg_hic_bed_file, output_dir):
+    output_dir = os.path.join(output_dir, "AvgHiC")
+    os.makedirs(output_dir, exist_ok=True)
+    file_handles = {}
+    with gzip.open(avg_hic_bed_file, "rt") as f:
+        for line in f:
+            if line.startswith("#"):  # header line
+                continue
+            chrom = line.split("\t")[0]
+
+            if chrom not in file_handles:
+                print(f"Writing lines for {chrom}")
+                os.makedirs(os.path.join(output_dir, chrom), exist_ok=True)
+                chrom_file = os.path.join(
+                    os.path.join(output_dir, chrom), f"{chrom}.bed"
+                )
+                file_handles[chrom] = open(chrom_file, "w")
+            file_handles[chrom].write(line)
+
+    # Close all file handles
+    for fh in file_handles.values():
+        filename = fh.name
+        cmd = f"pigz -f {filename}"
+        print(f"Gzipping {filename}")
+        subprocess.run(cmd, shell=True, check=True)
+        fh.close()
+
+
+if __name__ == "__main__":
+    main()
