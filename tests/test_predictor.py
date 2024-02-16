7a61b7a66849683af937e65c0662ba80d0214364
Anthony Tan <atan5133@gmail.com>
2024-02-16 10:38:10 -0800

    make sure we import hicstraw before pandas

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -2,11 +2,10 @@ import os
 import sys
 import unittest
 
-import pandas as pd
-
 SCRIPTS_DIR = os.path.abspath("workflow/scripts")
 sys.path.insert(0, SCRIPTS_DIR)
-from predictor import add_hic_from_hic_file, determine_num_rows_to_fetch
+from predictor import add_hic_from_hic_file
+import pandas as pd
 
 HIC_FILE = "https://www.encodeproject.org/files/ENCFF621AIY/@@download/ENCFF621AIY.hic"
 
