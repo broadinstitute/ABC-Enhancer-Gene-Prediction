29caf8ff5e0e5f2d72f705a4b00a28b12779c526
Maya Sheth <shethm@sh02-ln02.stanford.edu>
2025-04-25 12:50:25 -0700

    annotate predictions with selfgenic flag

--- workflow/scripts/predictor.py
+++ workflow/scripts/predictor.py
@@ -20,7 +20,7 @@ def make_predictions(
     chromosome, enhancers, genes, args, hic_gamma, hic_scale, chrom_sizes_map
 ):
     pred = make_pred_table(chromosome, enhancers, genes, args.window, chrom_sizes_map)
-    pred = annotate_predictions(pred, args.tss_slop)
+    pred = annotate_predictions(pred, genes, args.tss_slop)
     pred = add_powerlaw_to_predictions(pred, args, hic_gamma, hic_scale)
     # if Hi-C file is not provided, only powerlaw model will be computed
     if args.hic_file:
@@ -515,9 +515,8 @@ def compute_score(enhancers, product_terms, prefix, adjust_self_promoters=True):
 
     return enhancers
 
-
-def annotate_predictions(pred, tss_slop=500):
-    # TO DO: Add is self genic
+def annotate_predictions(pred, genes, tss_slop=500):
+    ## annotate self-promoter E-G pairs
     pred["isSelfPromoter"] = np.logical_and.reduce(
         (
             pred["class"] == "promoter",
@@ -525,6 +524,18 @@ def annotate_predictions(pred, tss_slop=500):
             pred.end + tss_slop > pred.TargetGeneTSS,
         )
     )
+
+    ## annotate self-genic E-G pairs
+    genes_subset = genes[['name', 'start', 'end']].copy()
+    pred = pred.merge(genes_subset, how='left', left_on='TargetGene', right_on='name', suffixes=('', '_gene'))
+
+    pred['isSelfGenic'] = np.logical_and.reduce((
+        pred['class'] == 'genic',
+        pred['start'] <= pred['end_gene'],
+        pred['end'] >= pred['start_gene'],
+    ))
+
+    pred.drop(columns=['name', 'start_gene', 'end_gene'], inplace=True)
 
     return pred
 
